a8e712e5ca5915c616fb1b1a98f913965eae37e7
Manel <manelhammouche3@gmail.com>
2022-06-02 12:43:34 +0100

    Create categories component

---
+++ src/components/Categories.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Categories = () => (
+  <section id="categories-page">
+    <h2>Categories</h2>
+    <button type="button">CHECK STATUS</button>
+  </section>
+);
+
+export default Categories;
