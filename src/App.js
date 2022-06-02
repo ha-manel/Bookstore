4fdbaba29037c2564d2a7bde401d3a966d7c3732
Manel <manelhammouche3@gmail.com>
2022-06-02 12:44:11 +0100

    Import components and create routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
+import {
+  Routes,
+  Route,
+} from 'react-router-dom';
 import './App.css';
+import BooksList from './components/BooksList';
+import Categories from './components/Categories';
+import Header from './components/Header';
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <>
+      <Header />
+      <main>
+        <Routes>
+          <Route path="/" element={<BooksList />} />
+          <Route path="/categories" element={<Categories />} />
+        </Routes>
+      </main>
+    </>
   );
 }
 
