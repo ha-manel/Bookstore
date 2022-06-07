179a4359edabcb0dc5cfbea30dc0cf0d1ec28950
Manel <manelhammouche3@gmail.com>
2022-06-07 11:19:29 +0100

    Dispatch action for showing status

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { checkStatus } from '../redux/categories/categories';
 
-const Categories = () => (
-  <section id="categories-page">
-    <h2>Categories</h2>
-    <button type="button">CHECK STATUS</button>
-  </section>
-);
+const Categories = () => {
+  const status = useSelector((state) => state.categories);
+  const dispatch = useDispatch();
+  const checkCatStatus = () => {
+    dispatch(checkStatus());
+  };
+
+  return (
+    <section id="categories-page">
+      <h2>Categories</h2>
+      <p>{status}</p>
+      <button type="button" onClick={() => checkCatStatus()}>CHECK STATUS</button>
+    </section>
+  );
+};
 
 export default Categories;
