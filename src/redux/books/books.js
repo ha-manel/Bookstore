7acfece6ecf0e584fb2a57e4f13a69545a963b81
Manel <manelhammouche3@gmail.com>
2022-06-07 11:20:08 +0100

    Update initial state of books list

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -3,7 +3,18 @@ const ADD_BOOK = 'bookstore/booksReducer/ADD_BOOK';
 const REMOVE_BOOK = 'bookstore/booksReducer/REMOVE_BOOK';
 
 // Reducer
-export default function booksReducer(state = [], action = {}) {
+export default function booksReducer(state = [
+  {
+    id: 1,
+    title: 'test1',
+    author: 'test1',
+  },
+  {
+    id: 2,
+    title: 'test2',
+    author: 'test2',
+  },
+], action = {}) {
   switch (action.type) {
     case ADD_BOOK:
       return [...state, action.payload];
